dad00335ac3e7732e6caecbd95ed5e4ed0ad0e7e
Eugene Tuhaise <genehaise@gmail.com>
2024-03-11 00:06:18 +0300

    Changes to admin file

--- SDproject/CustomerApp/admin.py
+++ SDproject/CustomerApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer,Orders,Feedback
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Orders, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
