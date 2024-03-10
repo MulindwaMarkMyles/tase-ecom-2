25141b030caf1212aec67dfb009dd02a35c0de44
Eugene Tuhaise <genehaise@gmail.com>
2024-03-11 00:13:02 +0300

    Changed project settings

--- SDproject/SDproject/settings.py
+++ SDproject/SDproject/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
