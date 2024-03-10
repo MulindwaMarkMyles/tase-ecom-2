ef345ccfe906b0552364efabf90a0b462115c498
Eugene Tuhaise <genehaise@gmail.com>
2024-03-11 00:01:02 +0300

    Changes to admin file

--- SDproject/BusinessApp/admin.py
+++ SDproject/BusinessApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Business,Product,Service,Feedback
 
-# Register your models here.
+
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Business, BusinessAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Service, ServiceAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
+
